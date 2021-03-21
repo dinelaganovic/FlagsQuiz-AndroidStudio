faec9b1cda77d255f0e2e60c29f03a480cad5847
Dinela <59210189+dinelaganovic@users.noreply.github.com>
2021-03-21 01:40:34 +0100

    Add files via upload

---
+++ java/com/example/flagsquiz/CountryItem.java
@@ -0,0 +1,17 @@
+package com.example.flagsquiz;
+
+public class CountryItem {
+    String name;
+    int image;
+    public CountryItem(String name, int image){
+        this.name=name;
+        this.image=image;
+
+    };
+    public  String getName() {
+        return  name;
+    };
+    public  int  getImage() {
+        return  image;
+    };
+}
---
+++ java/com/example/flagsquiz/Database.java
@@ -0,0 +1,28 @@
+package com.example.flagsquiz;
+
+public class Database {
+    Integer[] flags={
+            R.drawable.austrija,
+            R.drawable.egipat,
+            R.drawable.etiopija,
+            R.drawable.hrvatska,
+            R.drawable.nemacka,
+            R.drawable.nepal,
+            R.drawable.rusija,
+            R.drawable.spanija,
+            R.drawable.srbija,
+            R.drawable.texsas
+    };
+    String[] answers={
+            "austrija",
+            "egipat",
+            "etiopija",
+            "hrvatska",
+            "nemacka",
+            "nepal",
+            "rusija",
+            "spanija",
+            "srbija",
+            "texsas"
+    };
+}
